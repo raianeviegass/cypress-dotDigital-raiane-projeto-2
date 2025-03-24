8b0aa06b99f9c1c98b48d26f34a766d49b775d33
Raiane Viegas <raianeviegass@gmail.com>
2025-03-24 00:15:24 -0300

    Adicionando geração de relatório

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,22 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-
     baseUrl: 'https://www.mercadolivre.com.br/',
-
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: false,
+    retries: 2,
   },
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'mochawesome',
+    mochawesomeReporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: true,
+      html: true,
+      json: true,
+    }
+  }
+
 });
